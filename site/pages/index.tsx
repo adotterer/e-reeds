91edb0f0dac9607e48992c8cb216bfd0a5893c11
adotterer <adotterer@gmail.com>
2022-09-22 16:12:23 -0700

    Bam

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -39,6 +39,7 @@ export default function Home({
   products,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   console.log('Products ---->', products)
+  console.log('I only want to see the one hoodie')
   return (
     <>
       <Grid variant="filled">
